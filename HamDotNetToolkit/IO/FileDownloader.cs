10230c614cbd512e0f90dafc09380666bcc630b0
Darryl Wagoner <darryl@wagoner.me>
2023-12-14 06:48:43 -0600

    fcc ops

---
+++ HamDotNetToolkit/IO/FileDownloader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamDotNetToolkit.IO;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
+public class FileDownloader
+{
+    private readonly HttpClient client;
+
+    public FileDownloader()
+    {
+        client = new HttpClient();
+    }
+
+    public async Task DownloadFileAsync(string fileUrl, string filePath)
+    {
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(fileUrl, HttpCompletionOption.ResponseHeadersRead);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error: {response.StatusCode}");
+            }
+
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                await stream.CopyToAsync(fileStream);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+}
+
---
+++ HamDotNetToolkit/IO/ZipFileReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamDotNetToolkit.IO;
+
+public class ZipFileReader
+{
+    static string ReadFileFromZip(string zipFile, string fileName)
+    {
+        try
+        {
+            using ZipArchive archive = ZipFile.OpenRead(zipFile);
+            // Find the specific file in the ZIP
+            var entry = archive.GetEntry(fileName);
+            if (entry != null)
+            {
+                // Open a stream to the file
+                using Stream stream = entry.Open();
+
+                // Read the contents of the file
+                using StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+
+                string fileContents = reader.ReadToEnd();
+                return fileContents;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return string.Empty;
+    }
+}
